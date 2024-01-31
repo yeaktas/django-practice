a73519a9c4d4c929674390c8b122e3bcccd71b9b
Aktass <yunusemreaktas97@gmail.com>
2024-01-31 20:37:45 +0300

    day four: partial template, for

--- courses/urls.py
+++ courses/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('list', views.index),
     path('<kurs_adi>', views.details),
     path('kategori/<int:category_id>', views.getCoursesByCategoryId),
-    path('kategori/<str:category_name>', views.getCoursesByCategory),
+    path('kategori/<str:category_name>', views.getCoursesByCategory, name='courses_by_category'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
@@ -12,13 +13,53 @@ data = {
     "mobil":"mobil kategorisine ait kurslar",
 }
 
+db = {
+
+    "courses": [
+
+        {
+            "title": "javascript kursu",
+            "description": "javascript kursu aciklamasi",
+            "imageUrl": "https://img-c.udemycdn.com/course/750x422/1662526_fc1c_3.jpg",
+            "slug": "javascript-kursu",
+            "date": date(2024, 1, 1),
+            "is-active": True
+        },
+        {
+            "title": "python kursu",
+            "description": "python kursu aciklamasi",
+            "imageUrl": "https://img-c.udemycdn.com/course/750x422/2463492_8344_3.jpg",
+            "slug": "python-kursu",
+            "date": date(2024, 1, 1),
+            "is-active": False
+        },
+        {
+            "title": "web gelistirme kursu",
+            "description": "web gelistirme kursu aciklamasi",
+            "imageUrl": "https://img-c.udemycdn.com/course/750x422/1258436_2dc3_4.jpg",
+            "slug": "web-gelistirme-kursu",
+            "date": date(2024, 1, 1),
+            "is-active": True
+        }
+    ],
+
+    "categories": [
+        {"id": 1 , "name": "programlama", "slug": "programlama"}, 
+        {"id": 2 , "name": "web gelistirme", "slug": "web-gelistirme"}, 
+        {"id": 3 , "name": "mobil uygulamalar", "slug": "mobil-uygulamalar"}
+        ]
+}
+
+
 #http://127.0.0.1:8000/kurslar
 
 def index(request):
-    return render(request, 'courses/index.html')
-
-def list(request):
-    return HttpResponse('list')
+    kurslar = db["courses"]
+    kategoriler = db["categories"]
+    return render(request, 'courses/index.html', {
+        'categories': kategoriler,
+        'courses': kurslar,
+    })
 
 def details(request, kurs_adi):
     return HttpResponse(f'{kurs_adi} kurs detay sayfasi')
@@ -26,7 +67,10 @@ def details(request, kurs_adi):
 def getCoursesByCategory(request, category_name):
     try:
         category_text = data[category_name]
-        return HttpResponse(category_text)
+        return render(request, 'courses/courses.html', {
+            'category': category_name,
+            'category_text': category_text
+        })
     except:
         return HttpResponseNotFound('yanlis kategori secimi')
 
